1e3303a3968844a0f29dc7bfa3af631d8af929c3
rwightman <rick.wightman@q.com>
2015-07-23 21:09:36 -0600

    fix index.html src error by adding ng-

--- service.js
+++ service.js
@@ -1,12 +1,9 @@
-var app = angular.module('userProfiles');
-
-app.service('mainService', function($http) {
-
-
+angular.module('userProfiles')
+.service('mainService', function($http) {
   this.getUsers = function() {
     return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=3'
-    })
+      method: 'GET',
+      url: 'http://reqr.es/api/users?page=1'
+    });
   }
 });
